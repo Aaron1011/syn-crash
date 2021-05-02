accd9d2aafc53aaced8b8864626cb124c0c7cb91
Aaron Hill <aa1ronham@gmail.com>
2021-05-01 22:10:43 -0400

    Stub method

--- src/lit.rs
+++ src/lit.rs
@@ -38,31 +38,7 @@ pub mod parsing {
 
     impl Synom for Lit {
         fn parse(input: Cursor) -> PResult<Self> {
-            match input.literal() {
-                Some((rest, span, lit)) => {
-                    Ok((rest, Lit {
-                        span: Span(span),
-                        value: LitKind::Other(lit)
-                    }))
-                }
-                _ => match input.word() {
-                    Some((rest, span, sym)) => {
-                        let kind = if sym.as_str() == "true" {
-                            LitKind::Bool(true)
-                        } else if sym.as_str() == "false" {
-                            LitKind::Bool(false)
-                        } else {
-                            return parse_error();
-                        };
-
-                        Ok((rest, Lit {
-                            span: Span(span),
-                            value: kind
-                        }))
-                    }
-                    _ => parse_error(),
-                }
-            }
+            parse_error()
         }
     }
 }
