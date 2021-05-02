c31c528c6444c50337185b07f483738681e80521
Aaron Hill <aa1ronham@gmail.com>
2021-05-01 22:04:10 -0400

    Remove dead code

--- src/mac.rs
+++ src/mac.rs
@@ -18,7 +18,6 @@ ast_struct! {
     }
 }
 
-#[cfg_attr(feature = "clone-impls", derive(Clone))]
 pub struct TokenTree(pub proc_macro2::TokenTree);
 
 impl Mac {
@@ -36,97 +35,6 @@ impl TokenTree {
             TokenNode::Group(Delimiter::Brace, _) => true,
             _ => false,
         }
-    }
-}
-
-#[cfg(feature = "extra-traits")]
-impl PartialEq for TokenTree {
-    fn eq(&self, other: &TokenTree) -> bool {
-        use proc_macro2::Spacing;
-
-        match (&self.0.kind, &other.0.kind) {
-            (&TokenNode::Group(d1, ref s1), &TokenNode::Group(d2, ref s2)) => {
-                match (d1, d2) {
-                    (Delimiter::Parenthesis, Delimiter::Parenthesis) |
-                    (Delimiter::Brace, Delimiter::Brace) |
-                    (Delimiter::Bracket, Delimiter::Bracket) => {}
-                    (Delimiter::None, Delimiter::None) => {}
-                    _ => return false,
-                }
-
-                let s1 = s1.clone().into_iter();
-                let mut s2 = s2.clone().into_iter();
-
-                for item1 in s1 {
-                    let item2 = match s2.next() {
-                        Some(item) => item,
-                        None => return false,
-                    };
-                    if TokenTree(item1) != TokenTree(item2) {
-                        return false
-                    }
-                }
-                s2.next().is_none()
-            }
-            (&TokenNode::Op(o1, k1), &TokenNode::Op(o2, k2)) => {
-                o1 == o2 && match (k1, k2) {
-                    (Spacing::Alone, Spacing::Alone) |
-                    (Spacing::Joint, Spacing::Joint) => true,
-                    _ => false,
-                }
-            }
-            (&TokenNode::Literal(ref l1), &TokenNode::Literal(ref l2)) => {
-                l1.to_string() == l2.to_string()
-            }
-            (&TokenNode::Term(ref s1), &TokenNode::Term(ref s2)) => {
-                s1.as_str() == s2.as_str()
-            }
-            _ => false,
-        }
-    }
-}
-
-#[cfg(feature = "extra-traits")]
-impl Eq for TokenTree {}
-
-#[cfg(feature = "extra-traits")]
-impl ::std::hash::Hash for TokenTree {
-    fn hash<H: ::std::hash::Hasher>(&self, h: &mut H) {
-        use proc_macro2::Spacing;
-
-        match self.0.kind {
-            TokenNode::Group(delim, ref stream) => {
-                0u8.hash(h);
-                match delim {
-                    Delimiter::Parenthesis => 0u8.hash(h),
-                    Delimiter::Brace => 1u8.hash(h),
-                    Delimiter::Bracket => 2u8.hash(h),
-                    Delimiter::None => 3u8.hash(h),
-                }
-
-                for item in stream.clone().into_iter() {
-                    TokenTree(item).hash(h);
-                }
-                0xffu8.hash(h); // terminator w/ a variant we don't normally hash
-            }
-            TokenNode::Op(op, kind) => {
-                1u8.hash(h);
-                op.hash(h);
-                match kind {
-                    Spacing::Alone => 0u8.hash(h),
-                    Spacing::Joint => 1u8.hash(h),
-                }
-            }
-            TokenNode::Literal(ref lit) => (2u8, lit.to_string()).hash(h),
-            TokenNode::Term(ref word) => (3u8, word.as_str()).hash(h),
-        }
-    }
-}
-
-#[cfg(feature = "extra-traits")]
-impl fmt::Debug for TokenTree {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.0.to_string().fmt(f)
     }
 }
 
