e536c7199e48c1ba088a81651fa6d4a72b9a256e
Aaron Hill <aa1ronham@gmail.com>
2021-05-06 15:55:36 -0400

    Get closer

--- src/generics.rs
+++ src/generics.rs
@@ -6,6 +6,5 @@ ast_struct! {
     /// A lifetime definition, e.g. `'a: 'b+'c+'d`
     pub struct LifetimeDef {
         pub attrs: Vec<TokenTree>,
-        //pub bounds: Delimited<(), tokens::Add>,
     }
 }
