3ce2c20267fbca502758d9e138321f3cb73d0a86
Aaron Hill <aa1ronham@gmail.com>
2021-05-06 15:55:21 -0400

    Remove field

--- src/generics.rs
+++ src/generics.rs
@@ -6,6 +6,6 @@ ast_struct! {
     /// A lifetime definition, e.g. `'a: 'b+'c+'d`
     pub struct LifetimeDef {
         pub attrs: Vec<TokenTree>,
-        pub bounds: Delimited<(), tokens::Add>,
+        //pub bounds: Delimited<(), tokens::Add>,
     }
 }
