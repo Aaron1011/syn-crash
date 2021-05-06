f79e1c73ec44ba458ee2234512f0546a27eee666
Aaron Hill <aa1ronham@gmail.com>
2021-05-06 18:58:17 -0400

    Remove import

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -1,14 +1,4 @@
-use std::cmp::Ordering;
-use std::fmt::{self, Display};
-use std::hash::{Hash, Hasher};
-
-use proc_macro2::Term;
-use unicode_xid::UnicodeXID;
-
-use Span;
-
-pub struct Lifetime {
-}
+pub struct Lifetime {}
 
 #[cfg(feature = "printing")]
 mod printing {
