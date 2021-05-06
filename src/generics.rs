5549407c0fa95e9844c558d0f1a4b4800708c0a4
Aaron Hill <aa1ronham@gmail.com>
2021-05-06 15:52:03 -0400

    Inline field

--- src/generics.rs
+++ src/generics.rs
@@ -13,7 +13,7 @@ ast_struct! {
 ast_struct! {
     /// A lifetime definition, e.g. `'a: 'b+'c+'d`
     pub struct LifetimeDef {
-        pub attrs: Vec<Attribute>,
+        pub attrs: Vec<TokenTree>,
         pub bounds: Delimited<(), tokens::Add>,
     }
 }
