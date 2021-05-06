2a6bbdcfb32537299aa636a3444ef550c9672373
Aaron Hill <aa1ronham@gmail.com>
2021-05-06 15:54:57 -0400

    Remove generics

--- src/generics.rs
+++ src/generics.rs
@@ -1,14 +1,7 @@
 use super::*;
 use delimited::Delimited;
 
-ast_struct! {
-    /// Represents lifetimes and type parameters attached to a declaration
-    /// of a function, enum, trait, etc.
-    #[derive(Default)]
-    pub struct Generics {
-        pub lifetimes: Delimited<LifetimeDef, tokens::Comma>,
-    }
-}
+
 ast_struct! {
     /// A lifetime definition, e.g. `'a: 'b+'c+'d`
     pub struct LifetimeDef {
